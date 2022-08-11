84cd402314cbcd94915d606a6cf2706fca3a2f7b
Corey Psoinos <cpsoinos@primary.com>
2022-08-10 22:33:40 -0400

    Revert "env var casing"
    This reverts commit f14302839d3f91f5435d4240c0e0ec52822ec76c.

--- lighthouse-datadog-report/src/reportUtils.ts
+++ lighthouse-datadog-report/src/reportUtils.ts
@@ -4,7 +4,7 @@ import { scoresToDataPoints } from './datadogUtils'
 const JSON_REPORT_FILENAME_REGEXP = /lhr-\d+\.json$/
 
 export const extractReportFileNames = async () => {
-  const allReports = await fs.readdir(process.env['INPUT-RESULTS-PATH'] as string)
+  const allReports = await fs.readdir(process.env.INPUT_RESULTS_PATH as string)
   const jsonReports = allReports.filter((fileName: string) => {
     return JSON_REPORT_FILENAME_REGEXP.test(fileName)
   })
@@ -12,7 +12,7 @@ export const extractReportFileNames = async () => {
 }
 
 export const parseResults = async (fileName: string) => {
-  const filePath = `${process.env['INPUT-RESULTS-PATH']}/${fileName}`
+  const filePath = `${process.env.INPUT_RESULTS_PATH}/${fileName}`
   const report = await fs.readFile(filePath, 'utf8')
 
   const reportTimeStamp = fileName.match(/\d+/)?.[0]
